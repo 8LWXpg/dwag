922ada449154f7459f708846a439e1e5a25f767f
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-06-05 18:39:54 +0800

    zip release

--- dwag/release.ps1
+++ dwag/release.ps1
@@ -2,6 +2,7 @@ Push-Location $PSScriptRoot
 
 $name = 'dwag'
 $targetArch = @('win-x64', 'win-arm64')
+$tempDir = "./out/$name"
 
 $tag = Read-Host 'New tag'
 Set-Content -Path .\dwag.csproj -NoNewline ((Get-Content .\dwag.csproj -Raw) -replace '<Version>\d+\.\d+\.\d+\.0</Version>', "<Version>$tag.0</Version>")
@@ -14,9 +15,13 @@ foreach ($arch in $targetArch) {
     $releasePath = "./bin/Release/net9.0-windows/$arch/publish/$name.exe"
 
     dotnet publish -r $arch -c Release
-    Copy-Item $releasePath "./out/$name-$arch.exe"
+
+    Remove-Item "$tempDir/*" -Recurse -Force -ErrorAction Ignore
+    mkdir "$tempDir" -ErrorAction Ignore
+    Copy-Item $releasePath, ../README.md, ../CHANGELOG.md $tempDir -Recurse -Force
+    Compress-Archive "$tempDir" "./out/$name-$version-$arch.zip" -Force
 }
 
-gh release create v$tag --notes (parse-changelog ..\CHANGELOG.md) (Get-ChildItem ./out/*.exe)
+gh release create v$tag --notes (parse-changelog ..\CHANGELOG.md) (Get-ChildItem ./out/*.zip)
 
 Pop-Location
